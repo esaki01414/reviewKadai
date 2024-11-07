ab100dc89052ba050b9208fd2d6f048dc5575ea1
esaki01414 <2301138@s.asojuku.ac.jp>
2024-11-07 10:46:26 +0900

    都道府県の罫線引き

--- user/new_login.php
+++ user/new_login.php
@@ -63,12 +63,15 @@
                     <optgroup lavel="北海道地方"></optgroup>
                     <option value="北海道">北海道</option>
                     <hr>
+                    <optgroup lavel="東北地方"></optgroup>
                     <option value="青森県">青森県</option>
                     <option value="岩手県">岩手県</option>
                     <option value="宮城県">宮城県</option>
                     <option value="秋田県">秋田県</option>
                     <option value="山形県">山形県</option>
                     <option value="福島県">福島県</option>
+                    <hr>
+                    <optgroup lavel="関東地方"></optgroup>
                     <option value="茨城県">茨城県</option>
                     <option value="栃木県">栃木県</option>
                     <option value="群馬県">群馬県</option>
@@ -76,6 +79,8 @@
                     <option value="千葉県">千葉県</option>
                     <option value="東京都">東京都</option>
                     <option value="神奈川県">神奈川県</option>
+                    <hr>
+                    <optgroup lavel="中部地方"></optgroup>
                     <option value="新潟県">新潟県</option>
                     <option value="富山県">富山県</option>
                     <option value="石川県">石川県</option>
@@ -85,6 +90,8 @@
                     <option value="岐阜県">岐阜県</option>
                     <option value="静岡県">静岡県</option>
                     <option value="愛知県">愛知県</option>
+                    <hr>
+                    <optgroup lavel="近畿地方"></optgroup>
                     <option value="三重県">三重県</option>
                     <option value="滋賀県">滋賀県</option>
                     <option value="京都府">京都府</option>
@@ -92,15 +99,21 @@
                     <option value="兵庫県">兵庫県</option>
                     <option value="奈良県">奈良県</option>
                     <option value="和歌山県">和歌山県</option>
+                    <hr>
+                    <optgroup lavel="中国地方"></optgroup>
                     <option value="鳥取県">鳥取県</option>
                     <option value="島根県">島根県</option>
                     <option value="岡山県">岡山県</option>
                     <option value="広島県">広島県</option>
                     <option value="山口県">山口県</option>
+                    <hr>
+                    <optgroup lavel="四国地方"></optgroup>
                     <option value="徳島県">徳島県</option>
                     <option value="香川県">香川県</option>
                     <option value="愛媛県">愛媛県</option>
                     <option value="高知県">高知県</option>
+                    <hr>
+                    <optgroup lavel="九州地方"></optgroup>
                     <option value="福岡県">福岡県</option>
                     <option value="佐賀県">佐賀県</option>
                     <option value="長崎県">長崎県</option>
