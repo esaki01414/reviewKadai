efdc22092a4a862f706622a163efaebf85653943
hitoshibito <2301189@s.asojuku.ac.jp>
2024-11-08 10:47:31 +0900

    ログイン認証修正

--- admin/check.php
+++ admin/check.php
@@ -3,6 +3,7 @@
         $sid = isset($_POST['sid'])? htmlspecialchars($_POST['sid'], ENT_QUOTES, 'utf-8') : '';
         $password = isset($_POST['password'])? htmlspecialchars($_POST['password'], ENT_QUOTES, 'utf-8'): '';
 
+
         if ($sid == '') {
             header("Location:./index.php");
             exit;
@@ -14,7 +15,7 @@
 
         if ($sid=='222'&&$password=='Pass0708') {
             //ログイン許可
-            header("Location:./dashboard.php");
+            header("https://aso2301180.pinoko.jp/system/admin/dashboard.php");
         } else {
             //間違っているのでログイン不可
             header("Location:./index.php");
