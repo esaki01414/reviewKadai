b97cadc68c8fc3a5672c53fbb859d465a6f78135
hitoshibito <2301189@s.asojuku.ac.jp>
2024-11-12 12:56:38 +0900

    ダッシュボード完成

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -11,9 +11,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ダッシュボード</title>
+    <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/dashboard.css"> <!-- CSSファイルのリンク -->
+        <script src="https://kit.fontawesome.com/46129fa547.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/dashboard.css"> <!-- CSSファイルのリンク -->
 </head>
 <body>
     <header>
@@ -35,15 +37,15 @@
                 </div>
                 <div class="boxs">
                     <a href="product.php" class="box">
-                        <i class="far fa-newspaper icon"></i><!-- fontawesome利用部分 -->
+                    <i class="fa-solid fa-cube icon"></i><!-- fontawesome利用部分 -->
                         <p>商品管理</p>
                     </a>
                     <a href="G5.php" class="box">
-                        <i class="far fa-newspaper icon"></i><!-- fontawesome利用部分 -->
+                    <i class="fa-solid fa-users icon"></i><!-- fontawesome利用部分 -->
                         <p>ユーザー管理</p>
                     </a>
                     <a href="G3.php" class="box">
-                        <i class="far fa-newspaper icon"></i><!-- fontawesome利用部分 -->
+                    <i class="fa-solid fa-user-plus icon"></i><!-- fontawesome利用部分 -->
                         <p>新規登録</p>
                     </a>
                 </div>
