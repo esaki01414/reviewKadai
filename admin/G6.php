ce2aa4f7aef3708441dae8d864cd89ff9812b41b
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-11-12 12:20:17 +0900

    Create G6.php

---
+++ admin/G6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザー削除画面</title>
+</head>
+<body>
+    <h1>ユーザー削除</h1><br><br>
+    <form action="G7.php" method="post">   
+    ユーザーID<br>
+    <input type="text" name="D_id"><br><br><br>
+    <a href="G5.php">戻る</a>
+    <button type="submit">ユーザー削除</button>
+   </form>
+    </body>
+</html>
