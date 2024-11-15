e2bada09d8e203702a9ca5f7ff8bf9087e80c939
nakae-t <2301180@s.asojuku.ac.jp>
2024-11-15 09:14:02 +0900

    11月15日コミット編集

--- user/profile.php
+++ user/profile.php
@@ -99,15 +99,16 @@
                     <option value="宮崎県">宮崎県</option>
                     <option value="鹿児島県">鹿児島県</option>
                     <option value="沖縄県">沖縄県</option>
-<br>
-                    <label for="city">市区町村名</label>
+                    
+                </select><br>
+                    <label for="city">市区町村名</label><br>
                     <label for="sikutyoson">市区町村</label>
                 <input type="text" id="sikutyoson" name="user_address" placeholder="市区町村名を入力">
 <br>
                 <label for="street-address">番地・ビル名</label>
                 <input type="text" id="street-address" name="user_home_number"  placeholder="番地・ビル名を入力" required>
-               
-                <label for="phone-number">電話番号</label>
+               <br>
+                <label for="phone-number">電話番号</label><br>
                 <input type="tel" id="phone-number" name="user_tell" placeholder="000-0000-0000" pattern="\d{3}-\d{4}-\d{4}" required><br>
             <button type="submit" class="login-btn">新規登録</button>
             
