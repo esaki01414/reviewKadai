ad7f84cdfedac0987ff9ca4ebe067c802c1ec20f
esaki01414 <2301138@s.asojuku.ac.jp>
2024-11-21 12:22:22 +0900

    favorite.cssの作成とリンク付け

--- user/favorite.php
+++ user/favorite.php
@@ -41,6 +41,7 @@ $favorites = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/favorite.css">
     <title>お気に入り商品</title>
 </head>
 <body>
