9effdd5a92f4c643e7781b5a1b4bc3ea7b418f13
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-11-26 12:11:47 +0900

    Update G9.php

--- admin/G9.php
+++ admin/G9.php
@@ -34,7 +34,7 @@
        $stmt->execute([$id]);
        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         foreach($result as $row){     
-        echo '<img src="data:' .htmlspecialchars($row['image_type']).';base64,'.base64_encode($row['image_content']).'"height="200" alt="Product Image"><br>';
+        echo '<img src="data:' .htmlspecialchars($row['image_type']).';base64,'.base64_encode($row['image_content']).'"width="200" height="auto""><br>';
         echo '<p>商品ID:</p>';
         echo '<p>',htmlspecialchars($row['product_id']),'</p>';
         echo '<p>商品名:</p>';
