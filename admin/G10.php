99f2129a8cd6d8359e68ab1cbfc89c5193e9635c
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-11-28 11:59:06 +0900

    Update G10.php

--- admin/G10.php
+++ admin/G10.php
@@ -10,7 +10,24 @@
     <h1>商品更新</h1>
     <?php
     $id=$_POST['U'];
-    echo $id;
+    try {
+        $pdo = new PDO(
+            'mysql:host=mysql310.phy.lolipop.lan;dbname=LAA1554917-system;charset=utf8',
+            'LAA1554917',
+            'PassSD2D'
+        );
+    } catch (PDOException $e) {
+        echo 'データベース接続に失敗しました: ' . htmlspecialchars($e->getMessage());
+        exit;
+    }    
+    $sql='SELECT image_type,image_content FROM product WHERE product_id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($result as $row){
+    echo '<img src="data:'.htmlspecialchars($row['image_type']),';base64,',base64_encode($row['image_content']),'"width="200" height="auto""><br>';
+    }
     ?>
+
 </body>
 </html>
