da2c717d147861f07a972dee4d277d75628c78d3
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-03 21:36:16 +0900

    Update G11.php

--- admin/G11.php
+++ admin/G11.php
@@ -151,7 +151,8 @@ $data=[
 
     ?>
     <form action="G12.php" method="post" enctype="multipart/form-data">
-    <p><button type="submit" name="U[]" value="<?= htmlspecialchars(json_encode($data)) ?>">更新</button></p>
+    <input type="hidden" name="U" value="<?= htmlspecialchars(json_encode($data)) ?>">
+<p><button type="submit">更新</button></p>
 </form>
 </body>
 </html>
