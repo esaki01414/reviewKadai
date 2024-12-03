0cba117bbfa6c250f0dcedb6b052e0f1c1fff0a1
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-03 15:08:13 +0900

    Update G11.php

--- admin/G11.php
+++ admin/G11.php
@@ -18,6 +18,16 @@ if(!($id)){
 <a href="G10.php">戻る</a><br><br>
 <h1>商品更新確認</h1>
 <?php
+ try {
+    $pdo = new PDO(
+        'mysql:host=mysql310.phy.lolipop.lan;dbname=LAA1554917-system;charset=utf8',
+        'LAA1554917',
+        'PassSD2D'
+    );
+} catch (PDOException $e) {
+    echo 'データベース接続に失敗しました: ' . htmlspecialchars($e->getMessage());
+    exit;
+}  
 if(!empty($_POST['file'])){
     $flg='false';
     if(is_uploaded_file($_FILES['file']['tmp_name'])){
@@ -25,44 +35,73 @@ if(!empty($_POST['file'])){
             mkdir('upload_img');
         }
         $newfile ='upload_img/'.basename($_FILES['file']['name']);
-        $img_file_path=$newfile;
+        $imag=$newfile;
         $flg=move_uploaded_file($_FILES['file']['tmp_name'],$newfile);
     }
+}else{
+    $sql='SELECT image_type,image_content FROM product WHERE product_id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $imag = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
 if(!empty($_POST['name'])){
       $name =$_POST['name'];
+}else{
+    $sql='SELECT name FROM product WHERE product_id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $name=$stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
 if(!empty($_POST['size'])){
       $size=$_POST['size'];
+}else{
+    $sql='SELECT size FROM product WHERE product_id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $size=$stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
 if(!empty($_POST['stock'])){
       $stock=$_POST['stock'];
+}else{
+    $sql='SELECT stock FROM product WHERE product_id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $stock=$stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 if(!empty($_POST['color'])){
       $color=$_POST['color'];
+}else{
+    $sql='SELECT color FROM product WHERE product_id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $color=$stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 if(!empty($_POST['body'])){
       $body=$_POST['body'];
+}else{
+    $sql='SELECT body FROM product WHERE product_id = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $name=$stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 if(!empty($_POST['price'])){
       $price=$_POST['price'];
-}
-    try {
-        $pdo = new PDO(
-            'mysql:host=mysql310.phy.lolipop.lan;dbname=LAA1554917-system;charset=utf8',
-            'LAA1554917',
-            'PassSD2D'
-        );
-    } catch (PDOException $e) {
-        echo 'データベース接続に失敗しました: ' . htmlspecialchars($e->getMessage());
-        exit;
-    }    
-    $sql='SELECT * FROM product WHERE product_id = ?';
+}else{
+    $sql='SELECT name FROM product WHERE product_id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach($result as $row){
+    $name=$stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+    
+$result=[$name,$size,$stock,$color,$body,$price];
+    foreach($imag as $row){
     echo '<img src="data:'.htmlspecialchars($row['image_type']).';base64,'.base64_encode($row['image_content']).'"width="200" height="auto""><br>';
+    }
+    foreach($result as $row){
+    echo $row;
     }
     
     ?>
