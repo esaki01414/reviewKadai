f0ae3a3ca5d552a4421aebf90275f464cf47fd6d
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-03 16:50:46 +0900

    Create G12.php

---
+++ admin/G12.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品更新完了画面</title>
+</head>
+<body>
+<?php
+$data=$_POST['U'];
+?>
+<h1>更新完了</h1>
+<?php
+echo $data;
+?>
+<a href="G9.php">戻る</a>
+</body>
+</html>
