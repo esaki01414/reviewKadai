c2edec962334c732d1a8232127e4d73ff1b0c140
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-03 15:33:20 +0900

    Update G11.php

--- admin/G11.php
+++ admin/G11.php
@@ -43,67 +43,86 @@ if(!empty($_POST['file'])){
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
     $imag = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($imag as $row){
+        echo '<img src="data:'.htmlspecialchars($row['image_type']).';base64,'
+        .base64_encode($row['image_content']).'"width="200" height="auto""><br>';
+        }
 }
 
 if(!empty($_POST['name'])){
       $name =$_POST['name'];
+      echo $name;
 }else{
     $sql='SELECT product_name FROM product WHERE product_id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
     $name=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($name as $row){
+        echo $row['product_name'];
+        }
 }
 
 if(!empty($_POST['size'])){
       $size=$_POST['size'];
+      echo $size;
 }else{
     $sql='SELECT product_size FROM product WHERE product_id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
     $size=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($size as $row){
+        echo $row['product_size'];
+        }
 }
 
 if(!empty($_POST['stock'])){
       $stock=$_POST['stock'];
+      echo $stock;
 }else{
     $sql='SELECT inventory_stock FROM product WHERE product_id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
     $stock=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($stock as $row){
+        echo $row['inventory_stock'];
+        }
 }
 if(!empty($_POST['color'])){
       $color=$_POST['color'];
+      echo $color;
 }else{
     $sql='SELECT product_color FROM product WHERE product_id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
     $color=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($color as $row){
+        echo $row['product_color'];
+        }
 }
 if(!empty($_POST['body'])){
       $body=$_POST['body'];
+      echo $body;
 }else{
     $sql='SELECT product_body FROM product WHERE product_id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
-    $name=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    $body=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($size as $row){
+        echo $row['product_body'];
+        }
 }
 if(!empty($_POST['price'])){
       $price=$_POST['price'];
+      echo $price;
 }else{
     $sql='SELECT product_price FROM product WHERE product_id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
-    $name=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    $price=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($price as $row){
+        echo $row['product_price'];
+        }
 }
-    
-$result=[$name,$size,$stock,$color,$body,$price];
-    foreach($imag as $row){
-    echo '<img src="data:'.htmlspecialchars($row['image_type']).';base64,'.base64_encode($row['image_content']).'"width="200" height="auto""><br>';
-    }
-    foreach($result as $row){
-    echo $row;
-    }
-    
     ?>
 </body>
 </html>
