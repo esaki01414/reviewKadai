7d4a6820ce4e830f36b832cd54a862627bc5ec01
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-03 20:59:34 +0900

    更新

--- admin/G12.php
+++ admin/G12.php
@@ -20,7 +20,7 @@
 }  
 
 if (isset($_POST['U'])) {
-    $U = json_decode($_POST['U'], true);    // Now you can use the $result array
+    $U = $_POST['U'];    // Now you can use the $result array
 }
 for($i=0;$i<=7;$i++){
   echo $U['data'][$i];
