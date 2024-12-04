cba7727e1914a65e81a1592297d04ea436583b00
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-05 01:54:28 +0900

    Update G12.php

--- admin/G12.php
+++ admin/G12.php
@@ -36,15 +36,12 @@ if (isset($_POST['U']) && !empty($_POST['U'])) {
         $_POST['price'],
     ];
     echo 'ok';
-    foreach($data as $d){
-        echo $d;
-    }
     if(isset($_POST['imag1']) && !empty($_POST['imag1']) && isset($_POST['imag2']) && !empty($_POST['imag2']) &&
     isset($_POST['imag3']) && !empty($_POST['imag3']) && isset($_POST['imag4']) && !empty($_POST['imag4'])){
         echo 'ok';
     }elseif(isset($_POST['image_type']) && !empty($_POST['image_type']) && isset($_POST['image_content']) && !empty($_POST['image_content'])){
-        $image_type= htmlspecialchars($_POST['imag_type']);
-        $image_content= htmlspecialchars($_POST['image_content']);
+        $image_type= $_POST['image_type'];
+        $image_content= $_POST['image_content'];
         echo 'ok';
     }else{
         echo '<p>データが正常ではありません。</p>';
