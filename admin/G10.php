51f4c4a4fe832f6125c57f6d8309e98c370d4620
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-05 13:35:29 +0900

    Update G10.php

--- admin/G10.php
+++ admin/G10.php
@@ -49,7 +49,7 @@ if(!($id)){
     echo '<img src="data:'.htmlspecialchars($row['image_type']).';base64,'.base64_encode($row['image_content']).'"width="200" height="auto""><br>';
     }
     ?>
-    <form action="G11.php" method="post" enctype="multipart/form-data">
+    <form action="G11.php" class="edit-form" method="post" enctype="multipart/form-data">
         <p>商品名：</p>
         <input type="text" name="name">
         <p>サイズ</p>
