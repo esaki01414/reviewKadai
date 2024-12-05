b3012be04c6f019c8710313a3c7584e1e2e4a99b
hujpocinguha <2301221@s.asojuku.ac.jp>
2024-12-05 13:31:14 +0900

    Update G10.php

--- admin/G10.php
+++ admin/G10.php
@@ -8,6 +8,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>商品更新画面</title>
     <link rel="stylesheet" href="css/styles.css"> <!-- CSSファイルのリンク -->
+    <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="css/G13.css">
